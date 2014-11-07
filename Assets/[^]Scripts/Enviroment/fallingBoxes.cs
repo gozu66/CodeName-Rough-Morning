52353454df66517b7acfd007c9eb97bc1d999841
gozu66 <gozu66@gmail.com>
2014-11-07 13:23:48 +0000

    07.11.14_RB_13.23 "Added falling boxes functionality etc"

---
+++ Assets/[^]Scripts/Enviroment/fallingBoxes.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallingBoxes : MonoBehaviour 
+{
+	public GameObject fallCube;
+	public GameObject[] fallCubes;
+
+	Vector3 spawnPoint1, spawnPoint2, spawnPoint3;
+
+	float timeCounter;
+	public float respawnTime = 5;
+
+	void Start()
+	{
+		spawnPoint1 = fallCubes[0].transform.position;
+		spawnPoint2 = fallCubes[1].transform.position;
+		spawnPoint3 = fallCubes[2].transform.position;
+
+		//foreach()
+	}
+
+	void Update()
+	{
+		timeCounter += Time.deltaTime;
+
+		if(timeCounter >= respawnTime)
+		{
+			Respawn();
+		}
+	}
+
+	void Respawn()
+	{
+		Instantiate(fallCube, spawnPoint1, Quaternion.identity);
+		Instantiate(fallCube, spawnPoint2, Quaternion.identity);
+		Instantiate(fallCube, spawnPoint3, Quaternion.identity);
+
+		timeCounter = 0;
+	}
+}

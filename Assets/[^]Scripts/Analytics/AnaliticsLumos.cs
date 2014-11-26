cda3a45c2d66105d5bb9978fd2a0d0fbd21a067c
gozu66 <gozu66@gmail.com>
2014-11-26 02:07:01 +0000

    Lumos tweaks

--- Assets/[^]Scripts/Analytics/AnaliticsLumos.cs
+++ Assets/[^]Scripts/Analytics/AnaliticsLumos.cs
@@ -1,43 +1,43 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//
-//public class AnaliticsLumos : MonoBehaviour 
-//{
-//	float timer;
-//	int i;
-//
-//	void Start()
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnaliticsLumos : MonoBehaviour 
+{
+	float timer;
+	int i;
+
+	void Start()
+	{
+		timer = 0;
+		i = 1;
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+	}
+
+//	void OnTriggerEnter2D(Collider2D other)
 //	{
-//		timer = 0;
-//		i = 1;
-//	}
+//		Debug.Log("hit");
 //
-//	void Update()
-//	{
-//		timer += Time.deltaTime;
-//	}
-//
-////	void OnTriggerEnter2D(Collider2D other)
-////	{
-////		Debug.Log("hit");
-////
-////		if(other.tag == "checkPoint")
-////		{
-////			CheckPointHit();
-////			Debug.Log("hitCP");
-////		}
-////	}
-//
-//	void CheckPointHit()
-//	{
-//		if(i <= 7)
+//		if(other.tag == "checkPoint")
 //		{
-//			LumosAnalytics.RecordEvent("TestLevel", "CheckPoint " + i.ToString("0"), timer);
-//			i++;
-//
-//		}
-//		else{
-//			LumosAnalytics.RecordEvent("TestLevel", "Finish Time ", timer);
+//			CheckPointHit();
+//			Debug.Log("hitCP");
 //		}
 //	}
-//}
+
+	void CheckPointHit()
+	{
+		if(i <= 7)
+		{
+			LumosAnalytics.RecordEvent("TestLevel", "CheckPoint " + i.ToString("0"), timer);
+			i++;
+
+		}
+		else{
+			LumosAnalytics.RecordEvent("TestLevel", "Finish Time ", timer);
+		}
+	}
+}

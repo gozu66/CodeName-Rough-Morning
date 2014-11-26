eac5cc695a112f2b77fb83e523b7fd1eeae61646
gozu66 <gozu66@gmail.com>
2014-11-26 20:55:56 +0000

    Added M+KB prompts and UI controller logic

---
+++ Assets/[^]Scripts/Levels/ControllsUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllsUI : MonoBehaviour 
+{
+	public GameObject Pad;
+	public GameObject MKB;
+
+	void Start()
+	{
+		if(playerAim._input == playerAim.InputType.MouseKBoard)
+		{
+			Pad.SetActive(false);
+			MKB.SetActive(true);
+		}
+		else{
+			MKB.SetActive(true);
+			Pad.SetActive(false);
+		}
+	}
+}

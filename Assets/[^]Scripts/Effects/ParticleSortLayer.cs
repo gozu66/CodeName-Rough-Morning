56af7a857e72895b376758018b03cadff9e76892
gozu66 <gozu66@gmail.com>
2015-01-11 22:44:01 +0000

    2nd Alpha build, art assets added.

---
+++ Assets/[^]Scripts/Effects/ParticleSortLayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortLayer : MonoBehaviour {
+
+	void Start()
+	{
+		particleEmitter.renderer.sortingLayerName = "Foreground";
+	}
+}

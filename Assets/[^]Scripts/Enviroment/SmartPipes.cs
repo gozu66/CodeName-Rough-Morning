9951881a00e1b63f1cec08905432f1c95897eb94
gozu66 <gozu66@gmail.com>
2015-01-21 02:26:44 +0000

    Smart Pipes script first pass

--- Assets/[^]Scripts/Enviroment/SmartPipes.cs
+++ Assets/[^]Scripts/Enviroment/SmartPipes.cs
@@ -3,24 +3,60 @@ using System.Collections;
 
 public class SmartPipes : MonoBehaviour 
 {
-	GameObject[] SnapPoints;
+	GameObject[] endSnapPoint;
+	GameObject localSnapPoint, CurrEndSnap;
+
 	Transform myT;
+
+	float i = Mathf.Infinity;
 
 	public float MinSnapDistance = 0.2f, MinRotDistance = 5f;
 
 	void Start()
 	{
 		myT = transform;
-		SnapPoints = GameObject.FindGameObjectsWithTag("SP");
+		endSnapPoint = GameObject.FindGameObjectsWithTag("endSnapPoint");
+
+		localSnapPoint = this.transform.GetChild(0).gameObject;
+		if(localSnapPoint.tag == "endSnapPoint")
+			localSnapPoint = this.transform.GetChild(1).gameObject;
 	}
 
 	void SnapPipe()
 	{
-
+		foreach(GameObject point in endSnapPoint)
+		{
+			float dist = Vector2.Distance(localSnapPoint.transform.position, point.transform.position);
+			if(dist < i)
+			{
+				i = dist;
+				CurrEndSnap = point;
+			}
+		}
+		if(Compare() == true)
+		{
+			rigidbody2D.isKinematic = true;
+			collider2D.isTrigger = true;
+		}
 	}
 
-	Vector3 CompareDist(GameObject x, Transform y)
+	bool Compare()
 	{
-		return Vector3.zero;
+		float dist = Vector2.Distance(localSnapPoint.transform.position, CurrEndSnap.transform.position);
+		if(dist <= MinSnapDistance)
+		{
+			float rotOffset = Quaternion.Angle(CurrEndSnap.transform.rotation, localSnapPoint.transform.rotation);
+			Debug.Log(rotOffset);
+
+			if(rotOffset < MinRotDistance)
+			{
+				return true;
+			}else
+			{
+				return false;
+			}
+		}else{
+		return false;
+		}
 	}
 }

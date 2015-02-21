a79823aab7d9ad89c1841521b621d2c744db7566
gozu66 <gozu66@gmail.com>
2015-02-21 21:54:33 +0000

    Audio Visualizer first pass

---
+++ Assets/[^]Scripts/Effects/AudioVisualiser2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioVisualiser2 : MonoBehaviour 
+{
+	LineRenderer myLine;
+	public int resolution, multiplier;
+
+	void Start()
+	{
+		myLine = GetComponent<LineRenderer>();
+		InvokeRepeating("Visualize", 0.0f, 0.025f);
+	}
+
+	void Visualize()
+	{
+		float[] audioData = new float[resolution];
+		AudioListener.GetOutputData(audioData, 0);
+		myLine.SetVertexCount(audioData.Length);
+		for(int i = 0; i < audioData.Length; i++)
+		{
+			myLine.SetPosition(i, new Vector3(i, (audioData[i] * multiplier), 0));
+		}
+	}
+}

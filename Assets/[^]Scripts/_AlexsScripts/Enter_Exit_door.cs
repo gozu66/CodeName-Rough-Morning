eca8e4f6355ce948c386065ba4e6bd49132cae2d
Alex Chepyha <chepigas@ukr.net>
2015-04-23 00:05:12 +0100

    Tags and script was added

---
+++ Assets/[^]Scripts/_AlexsScripts/Enter_Exit_door.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enter_Exit_door : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject.tag == "Untagged") 
+				{
+					//transform.position = new Vector3(0, 0, 0);
+					Debug.Log("Hello world");
+				}
+		}
+
+}
